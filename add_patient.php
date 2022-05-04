314b290f38232fcfe67251941e0dd284570bbafd
Marc Shebaby <marcshababy02@gmail.com>
2022-05-05 01:23:47 +0300

    update insert

--- add_patient.php
+++ add_patient.php
@@ -6,48 +6,54 @@ $phone=$_POST['phone'];
 $gender=$_POST['gender'];
 $date=$_POST['date'];
 $type_of_test=$_POST['type_of_test'];
-$tumor_name=$POST['tumor_name'];
+$tumor_name=$_POST['tumor_name'];
 
-$stmt2=$con->prepare("SELECT id from patients WHERE email=? ");
+$stmt2=$con->prepare("SELECT id FROM patients WHERE email=? ");
 		$stmt2->bind_param("s", $email);
 		$stmt2->execute();
 		$patient_id=0;
 		$results = $stmt2->get_result();
-		$count = $results->num_rows(); 
-		if ($count >0){
-		
+
+		if ($results->num_rows >0){
+			
 			$stmt2->close();
 		}
 		else{
 			$stmt2->close();
-		$stmt = $con->prepare("insert into patients(name, email, phone, gender,hospital_id) values(?, ?, ?, ?,1");
-		$stmt->bind_param("sssss", $name, $email, $phone, $gender);
+		$stmt = $con->prepare("insert into patients(name, email, phone_number, gender,hospital_id) values(?, ?, ?, ?,1)");
+		$stmt->bind_param("ssss", $name, $email, $phone, $gender);
 		 $stmt->execute();
-		$patient_id = $stmt>insert_id;
+		 $stmt->close();
+		
+		
+		
 		
 	
-		$stmt->close();
+
 		}
 		
 		if($type_of_test='tumor'){
-			
-		$stmt=$con->prepare("insert into tumor_testing_orders(cost, date ,tumor_name, patient_id) values(50.5, ?, ?, ?");
+		$patient_id = "SELECT id FROM patients WHERE email='$email'";
+		
+
+		$rslt=mysqli_query($con, $patient_id);
+		$row=mysqli_fetch_row($rslt);
+		$stmt=$con->prepare("insert into tumor_testing_orders(cost, date ,tumor_name, patients_id) values(50.5, ?, ?, ?)");
 		 
 	
-		$stmt->bind_param("ssi",$date,$tumor_name,$patient_id);
+		$stmt->bind_param("ssi",$date,$tumor_name,$row[0]);
 		$stmt->execute();
 		$stmt->close();
 		}
 		else{
-			$stmt=$con->prepare("insert into sequence_testing_orders(date ,cost,patient_id) values(?, 50.5, ?");
-			$stmt->bind_param("si",$date,$patient_id);
+			$stmt=$con->prepare("insert into sequence_testing_orders(date ,cost,patients_id) values(?, 50.5, ?");
+			$stmt->bind_param("si",$date,$row[0]);
 		$stmt->execute();
 		$stmt->close();
 			
 		}
-		header('location index2.php');
-		?>
-	<!--	require "vendor/autoload.php";
+		header('location:index2.php');
+	/*	require "vendor/autoload.php";
 $robo = '.com';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -82,7 +88,7 @@ try {
 		
 		
 	
-		} -->
+		} */
 	
 	
-
+?>
