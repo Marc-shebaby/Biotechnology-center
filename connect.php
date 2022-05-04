4e527cb2ce31acd7f2db1276ca6bb03d113ec169
Marc Shebaby <marcshababy02@gmail.com>
2022-05-04 14:03:11 +0300

    Website update

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+$db_host="localhost";
+$db_user="root";
+$db_pass=null;
+$db_database="medilab";
+$mysqli=new mysqli($db_host,$db_user,$db_pass, $db_database);
+
+$con = mysqli_connect($db_host, $db_user, $db_pass, $db_database);
+
+?>
